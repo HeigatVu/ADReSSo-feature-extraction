016231faef64bca34156aec4c9f7f90e47632fde
HeigatVu <vnmduc.work@gmail.com>
2026-02-27 10:15:44 +0700

    finish transcription

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,20 @@
 import os
 from typing import Dict, List
+from pathlib import Path
 
 import pandas as pd
 import torch
 from transformers import pipeline
 from tqdm.auto import tqdm
+import glob
 
 def get_audio_files(audio_path:str) -> dict[str, List[str]]:
     """Load all audio files from ADReSSo structure
     """
 
     audio_files = {
-        "ad": sorted((audio_path/"ad").glob("*.wav")),
-        "cn": sorted((audio_path/"cn").glob("*.wav")),
+        "ad": sorted((Path(audio_path) / "ad").glob('*.wav')),
+        "cn": sorted((Path(audio_path) / "cn").glob('*.wav')),
     }
 
     print(f"Found {len(audio_files["ad"])} AD files")
