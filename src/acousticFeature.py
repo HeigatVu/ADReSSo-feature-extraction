515ea6bd1dbd0111d0386613e69755ff5092983a
HeigatVu <vnmduc.work@gmail.com>
2026-02-28 00:43:41 +0700

    starting build acoustic feature

---
+++ src/acousticFeature.py
@@ -0,0 +1,46 @@
+import math
+import statistics
+import numpy as np
+import parselmouth
+from parselmouth.praat import call
+
+# Prosody and Fluency
+def get_intensity_attributes():
+    pass
+
+def get_pitch_attributes():
+    pass
+
+def get_speaking_rate():
+    pass
+
+# Voice Quality and Phonation
+def get_harmonics_to_noise_ratio_attributes():
+    pass
+
+def get_glottal_to_noise_ratio_attributes():
+    pass
+
+def get_local_jitter():
+    pass
+
+def get_local_shimmer():
+    pass
+
+# Spectral and Articulatory
+def get_spectrum_attributes():
+    pass
+
+def get_formant_attributes():
+    pass
+
+
+# Cepstral (Timbral)
+def get_mfcc():
+    pass
+
+def get_lfcc():
+    pass
+
+def get_delta():
+    pass
